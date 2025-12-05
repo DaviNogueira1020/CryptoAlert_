2cd9b2c1bec4bf9a8ab7a3ce289a4f6f74243341
Davi Nogueira <davinogueiradasilvan@gmail.com>
2025-12-05 01:14:26 -0300

    fix: pin react and react-dom to exact version 19.2.0

--- apps/backend/dist/app.js
+++ apps/backend/dist/app.js
@@ -1,4 +1,37 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -14,7 +47,7 @@ const rateLimit_middleware_1 = require("./middlewares/rateLimit.middleware");
 const logger_1 = require("./utils/logger");
 // Swagger
 const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
-const openapi_json_1 = __importDefault(require("../docs/openapi.json"));
+const openapi = __importStar(require("../docs/openapi.json"));
 const alerts_new_routes_1 = __importDefault(require("./routes/alerts-new.routes"));
 // Coins routes
 const coins_routes_1 = __importDefault(require("./routes/coins.routes"));
@@ -47,7 +80,7 @@ app.use("/notifications", notificationRoutes);
 // Rotas de Usuários
 app.use("/users", usersRoutes);
 // Swagger UI
-app.use("/docs", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(openapi_json_1.default));
+app.use("/docs", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(openapi));
 // 404 handler
 app.use((req, res) => {
     res.status(404).json({
